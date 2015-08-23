892670294df8ab9c2571378d165a5f6c7d4165cc
uniQuk <uniQuk@users.noreply.github.com>
2015-08-23 12:06:45 +0100

    Update isMobileNumber.py
    * Shortened and enhanced the RE
    * Finds all matches and inserts them into a list

--- isMobileNumber.py
+++ isMobileNumber.py
@@ -1,12 +1,18 @@
 import re
 
-# TODO: Only match correct length.
-isMobileNumber = re.compile(r"07\d{9}|447\d{10}")
+
+matches = []
+# Only match correct length.
+
+isMobileNumber = re.compile(r'((\+?447|07)\d{9})[ ]')
 
 # TODO: Read in a file, local drive and extract mobile numbers
 message = input("> ")
 output = isMobileNumber.findall(message)
 
-# TODO: output file names and lines number is located on
+for hits in output:
+    matches.append(hits[0])
 
-print(output)
+# TODO: output file names and lines number is located on (currently N/A)
+
+print(matches)
