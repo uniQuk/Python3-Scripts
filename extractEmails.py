36873d4598b83995b69e77ceb8cebe0aef294df4
uniQuk <uniQuk@users.noreply.github.com>
2015-08-23 18:41:37 +0100

    Update extractEmails.py
    * Added user-agent string
    * Now works with https URLs without breaking

--- extractEmails.py
+++ extractEmails.py
@@ -1,8 +1,15 @@
 import re
-from urllib.request import urlopen
+from urllib.request import FancyURLopener
 from bs4 import BeautifulSoup
 
-html = urlopen(input("> "))
+
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+
+url = input("> ")
+
+myopener = MyOpener()
+html = myopener.open(url)
 
 soup = BeautifulSoup(html, "html.parser")
 
@@ -21,11 +28,7 @@ matches = []
 for groups in emailREX.findall(hits):
     matches.append(groups[1])
 
-# TODO: Make this more efficient
-# Move list to set (remive duplicates)
 set = set(matches)
-# Convert back to list
 result = list(set)
-
-# TODO: Send results to clipboard
+# print(hits)
 print(result)
